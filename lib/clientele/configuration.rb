3a47efc422f56ca70ee3f0175ae72c93c411dca1
Chris Keele <dev@chriskeele.com>
2014-09-30 15:20:34 -0500

    Allow auto-hashification of response bodies regex to be configurable.

--- lib/clientele/configuration.rb
+++ lib/clientele/configuration.rb
@@ -5,12 +5,13 @@ require 'faraday'
 module Clientele
   class Configuration < BlockParty::Configuration
 
-    attr_accessor :logger, :adapter, :headers, :root_url
+    attr_accessor :logger, :adapter, :headers, :hashify_content_type, :root_url
 
     def initialize
-      self.logger   =  Logger.new($stdout)
-      self.adapter  =  Faraday.default_adapter
-      self.headers  =  {}
+      self.logger               =  Logger.new($stdout)
+      self.adapter              =  Faraday.default_adapter
+      self.headers              =  {}
+      self.hashify_content_type = /\bjson$/
     end
 
   end
